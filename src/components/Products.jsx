f40a6fe53409ecab90e6319fe31269805f517d89
Dickens  Omondi <omondidickens255@gmail.com>
2025-02-06 20:29:46 +0300

    changed grid

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,7 +5,7 @@ export const Products = () => {
   return (
     <div className='mt-15 product'>
       <h2 className='text-3xl text-orange-950 font-extrabold'>Gallery</h2>
-      <div className='grid grid-cols-2 gap-2 ring-pink-600 md:grid-cols-3'>
+      <div className='grid grid-cols-2 gap-2 ring-pink-600 md:grid-cols-4'>
       <img  src="/public/img/chicken.jpg" alt="chicken" className='chicken-img' />
       <img src="/public/img/eggs1.jpg" alt="eggs" className='chicken-img' />
       <img src="/public/img/cock.jpg" alt="a bog hen" className='chicken-img' />
