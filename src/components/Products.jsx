4f62993c3947e75494a87a754bb3713162ca6c0d
dickens omondi <163810649+dikie001@users.noreply.github.com>
2025-02-15 19:01:44 +0300

    Update Products.jsx
    updated img

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,11 +6,11 @@ export const Products = () => {
     <div className='mt-15 product'>
       <h2 className='text-3xl text-orange-950 font-extrabold'>Gallery</h2>
       <div className='grid grid-cols-2 gap-2 ring-pink-600 md:grid-cols-4'>
-      <img  src="/public/img/chicken.jpg" alt="chicken" className='chicken-img' />
-      <img src="/public/img/eggs1.jpg" alt="eggs" className='chicken-img' />
-      <img src="/public/img/cock.jpg" alt="a bog hen" className='chicken-img' />
-      <img src="/public/img/eggs2.jpg" alt="eggs" className='chicken-img' />
-      <img src="/public/img/eggs3.jpg" alt="eggs" className='chicken-img' />
+      <img  src="/img/chicken.jpg" alt="chicken" className='chicken-img' />
+      <img src="/img/eggs1.jpg" alt="eggs" className='chicken-img' />
+      <img src="/img/cock.jpg" alt="a bog hen" className='chicken-img' />
+      <img src="/img/eggs2.jpg" alt="eggs" className='chicken-img' />
+      <img src="/img/eggs3.jpg" alt="eggs" className='chicken-img' />
       </div>
 
      <li className='order'><Link to="/order">Make an Order</Link></li>
